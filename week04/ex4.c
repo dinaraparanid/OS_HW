e2f6581459f4dcd3007f8dac81bed8e2651848d0
dinaraparanid <dinaraparanid@gmail.com>
2023-10-02 18:14:40 +0300

    background processed for ex4

--- week04/ex4.c
+++ week04/ex4.c
@@ -4,7 +4,8 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-const size_t MAX_COMMAND_SIZE = 1000;
+const size_t MAX_LINE_SIZE = 500;
+const size_t MAX_COMMAND_SIZE = 100;
 
 void trim_string(char* const str) {
     str[strcspn(str, "\n")] = '\0';
@@ -12,37 +13,48 @@ void trim_string(char* const str) {
 
 int main() {
     while (1) {
+        putchar('$'); putchar(' ');
+        char line[MAX_LINE_SIZE];
+        fgets(line, MAX_LINE_SIZE, stdin);
+
+        char* tok = strtok(line, " ");
+
+        char command[MAX_COMMAND_SIZE];
+        sprintf(command, "%s", "/bin/");
+        strcpy(command + 5, tok);
+        trim_string(command);
+
+        size_t new_argc = 0;
+        char** new_argv = malloc(sizeof(char*));
+
+        int is_background = 0;
+
+        while (tok != NULL) {
+            char* arg = malloc(strlen(tok) + 1);
+            strcpy(arg, tok);
+            trim_string(arg);
+
+            if (strcmp(arg, "&") == 0) {
+                is_background = 1;
+                free(arg);
+                break;
+            }
+
+            new_argv[new_argc++] = arg;
+            tok = strtok(NULL, " ");
+        }
+
         const pid_t child = fork();
 
         if (!child) {
-            putchar('$'); putchar(' ');
-            char line[MAX_COMMAND_SIZE];
-            fgets(line, MAX_COMMAND_SIZE, stdin);
-
-            char* tok = strtok(line, " ");
-
-            char command[200];
-            sprintf(command, "%s", "/bin/");
-            strcpy(command + 5, tok);
-            trim_string(command);
-
-            size_t new_argc = 0;
-            char** new_argv = malloc(sizeof(char*));
-
-            while (tok != NULL) {
-                new_argv = realloc(new_argv, ++new_argc * sizeof(char*));
-                new_argv[new_argc - 1] = malloc(strlen(tok) + 1);
-                strcpy(new_argv[new_argc - 1], tok);
-                trim_string(new_argv[new_argc - 1]);
-                tok = strtok(NULL, " ");
-            }
-
             execv(command, new_argv);
             exit(EXIT_FAILURE);
+        } else {
+            if (!is_background) {
+                int status = 0;
+                waitpid(child, &status, 0);
+            }
         }
-
-        int status = 0;
-        waitpid(child, &status, 0);
     }
 
     return 0;
